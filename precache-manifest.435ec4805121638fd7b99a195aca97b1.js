e427886420a767f41b558a4675e25df819f2efd2
Adilbek <ssadilbek@gmail.com>
2019-03-05 15:24:54 +0600

    Updates

---
+++ precache-manifest.435ec4805121638fd7b99a195aca97b1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "971bdc2f1971f74e29a3",
+    "url": "/react-gh-pages/static/js/main.971bdc2f.chunk.js"
+  },
+  {
+    "revision": "b58eb74f2e0d43dbf114",
+    "url": "/react-gh-pages/static/js/1.b58eb74f.chunk.js"
+  },
+  {
+    "revision": "1cfe9aefb2bb3d8a99263471e5a82a27",
+    "url": "/react-gh-pages/index.html"
+  }
+];
